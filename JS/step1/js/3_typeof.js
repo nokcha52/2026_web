a86cdfedf6783f319e31d1c3c52220b4925bc0be
nokcha52 <wltn7952@gmail.com>
2026-04-28 10:47:28 +0900

    형변환 진행중

--- JS/step1/js/3_typeof.js
+++ JS/step1/js/3_typeof.js
@@ -33,3 +33,27 @@ function testTypeof(){
     var favorNumber = 1;
     console.log(typeof(favorNumber));
 }// 자바스크립트 mdn: 사이트
+
+// case 2. 자신의 영어 이름을 engName 변수에 담아 자료형 check.
+function textTypeof(){
+    var engName = "wltn"
+    console.log(typeof(engName));
+}
+
+// case 3. 현재 자신이 춥다(true), 춥지 않다(false)를 cold 변수에 담아 자료형을 check.
+function check1(){
+    var cold = false;
+    console.log("추위를 느끼고 있나?" + cold + "(" + typeof(cold) + ")");
+}
+
+// case 4. obj 변쉥 객체를 담을 예정이라 null로 초기화하고 자료형을 check.
+function check2(){
+    var obj = null;
+    console.log("obj에 담긴 값의 자료 유형은" + typeof(obj) + "입니다.");
+}
+
+// case 5. test 변수를 선언하고 초기화하지 않은 상태에서 자료형을 check.
+function check3(){
+    var test;
+    console.log("초기화하지 않은 변수의 자료 유형은" + typeof(test) + "입니다.")
+}
---
+++ JS/step1/js/4_typeofChange.js
@@ -0,0 +1,131 @@
+/*
+    ? 형변환
+    * JS - "10" + "10" = 1010 / 사람 - "10" + "10" = 20 - 이런 인식차를 해결하기위함.
+    ! 암시적형변환 - js Engine이 자동으로 변환.  /  명시적 형변환 - 개발자가 직접 지정 변환.
+
+    ? 암시적 형변환 CASE
+    * 1. 숫자 + 문자 = 문자.
+    * 2. 불린 + 숫자 = 숫자.
+    * 3. 불린 + 문자 = 문자.
+    
+    ? 명시적 형변환
+    * method 나 lib Core func를 이용하여 강제로 숫자 -> 문자로 변환.
+*/
+// case 1. 자신의 나이를 입력받고 20을 더한 값을 출력.
+function agePlus(){
+    var age = window.prompt("나이를 입력하시오.");
+    var result = parseInt(age) + 20; //입력, 출력 변수를 다르게 설정한다.
+    console.log("20년뒤 나이는 " + result + "세입니다.")
+    console.log(typeof(age),typeof(result));
+}
+// case 2. 강제 숫자 - 문자형 변환.
+function forcedString(){
+    var a = '30';
+    var result = 1 + a + 10;
+    console.log("result의 자료유형은? " + typeof(result) + " 값은? " + result);
+}
+
+// case 3. 강제 불리언 - 숫자형 변환.
+function forcedNumber(){
+    var result = 2 + false; //? 불리언 형태를 숫자와 연산이 가능하다.
+    console.log("result의 자료 유형은? " + typeof(result) + " 값은? " + result);
+}
+
+// case 4. 강제 불리언 - 문자형 변환.
+function forcedString2(){
+    var a = "2";
+    var result = a + true;
+    console.log("result의 자료 유형은? " + typeof(result) + " 값은? " + result);
+}
+
+// case 5. test 1에 들어있는 숫자를 문자로 명시적 형변환.
+/*
+    ? String 
+    * String(value); - 문자형으로 변환
+    객체타입,전역함수/ 대문자로 시작하면 제일 먼저 발생한 함수
+
+    ? .toString()
+    *Number.toStiring(); - 문자형(진수)변환
+
+    ? .toFixed()
+    * Number.toFixed(); - (소수점자리수(반올림 발생) 실수문자형 변환)
+*/ 
+
+function stringNumb(){
+    var test1 = 15;
+    console.log("변환 전 : " + typeof(test1) + "=" + test1);
+    var result = String(test1);
+    console.log("변환 후 : " + typeof(result) + "=" + result);
+}
+function stringtoNumb(){
+    var test1 = 15;
+    console.log("변환 전 : " + typeof(test1) + "=" + test1);
+    var result = test1.toString();
+    console.log("변환 후 : " + typeof(result) + "=" + result);
+}
+function bgColorRamdom(){
+    setInterval(()=>{
+        var bgColor = Math.random()*0xfff;
+        // ? 0x = 16진을 의미, = 15*16^3 + 15*16^2 + 15*16^1 + 15*16^0 - 최대 수치 
+        // ! 2진 표현시 0000 0000 0000 0000 1111 1111 1111 111....-> 실사용불가 -> 변환
+        console.log("16진 랜덤 추출: " + bgColor);
+        bgColor = parseInt(bgColor);
+        console.log("정수형변환 : " + bgColor);
+        bgColor = bgColor.toString(16); //? 10이상 a ~ 15일 경우 f
+        console.log("16진 변환:" + bgColor);
+        document.querySelector("body").style.backgroundColor="#" + bgColor;
+    },1000);
+}
+
+function fixtoNumb(){
+    var pi = 3.14159265;
+    console.log("변환 전: " + typeof(pi) + "=" + pi);
+    var result = pi.toFixed(2);
+    console.log("변환 후 : " + typeof(result) + "=" + result);
+}
+// case 6. test에 들어있는 문자를 숫자로 형변환.
+/*
+    ?Number
+    * Numver(value); - 정수, 실수 구분 없이 형변환
+
+    ? parse
+    * 1. parseInt(value); - 정수형변환
+    * 2. parseFloat(value); - 실수형변환
+*/ 
+function numbString(){
+    var test = '300.15625'
+    console.log("변환 전: " + typeof(test) + "=" + test);
+    var result = Number(test) + 10;
+    console.log("변환 후: " + typeof(result) + "=" + result);
+}
+function numbString1(){
+    var test = '300.15625'
+    console.log("변환 전: " + typeof(test) + "=" + test);
+    var result = parseInt(test) + 10;
+    console.log("변환 후: " + typeof(result) + "=" + result);
+}
+function numbString2(){
+    var test = '300.15625'
+    console.log("변환 전: " + typeof(test) + "=" + test);
+    var result = parseFloat(test) + 10;
+    console.log("변환 후: " + typeof(result) + "=" + result);
+}
+
+// todo. 1-1 암시적형변환
+function todo1(){
+    var test = 100;
+    var result = test + ''; // todo
+    console.log(typeof(result) + "=" + result); //! string = 100
+}
+// todo. 1-2 String 형변환
+function todo2(){
+    var test = 100;
+    var result = String(test); // todo
+    console.log(typeof(result) + "=" + result); //! string = 100
+}
+// todo. 1-3 toString형변환
+function todo3(){
+    var test = 100;
+    var result = test.toString(); // todo
+    console.log(typeof(result) + "=" + result); //! string = 100
+}
