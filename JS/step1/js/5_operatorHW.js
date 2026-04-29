7c44c84dbddf6d5cd59e2570ba0cdfa7c976d031
nokcha52 <wltn7952@gmail.com>
2026-04-29 11:02:33 +0900

    과제완

---
+++ JS/step1/js/5_operatorHW.js
@@ -0,0 +1,17 @@
+// todo.1 자신의 이름을 한 글자씩 나누어 변수에 담은 후, 다시 하나의 변수에 담아 출력.
+function nameOperator(){
+    var a = '김';
+    var b = '지';
+    var c = '수';
+    console.log("내 이름은 : " + a + b + c);
+}
+
+// todo.2 다음 숫자가 출력 되도록 연산자를 활용하여 코드를 작성.
+
+function numberOperator(){
+    var number = 10;
+    console.log(number++);//? - 10
+    console.log(++number);//? - 12
+    console.log(number--);//? - 12
+    console.log(--number);//? - 10
+}
