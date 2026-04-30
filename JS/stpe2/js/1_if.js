2415375096204f6bcc0758ff022d127ae065d06a
nokcha52 <wltn7952@gmail.com>
2026-04-30 10:51:10 +0900

    if문 수업중

---
+++ JS/stpe2/js/1_if.js
@@ -0,0 +1,88 @@
+/*
+    조건문
+    
+    실무사용의 예
+    1. 로그인 시 아이디와 패스워드가 서버에 저장된 정보와 같은지 비교 후 같다면 승인, 아니라면 오류메세지를 띄운다.
+    2. 게임 같은 경우 자신이 공격을 했을 때 상대방을 타격 했는지 여부를 판별할 수 있다.
+    3. 경품 추첨시 유저가 뽑은 번호에 따라 경품 당첨 여부를 따질 수 있다.
+    4. 삭제 작업 수행시 사용자가 yes를 눌렀을 때와 no 를 눌렀을 때 수행할지 말지를 결정할 수있다.
+    5. slide 같은 ui component의 좌,우 방향으로 최대치로 이동 하였는지 판별할 수있다.
+
+	if(273 < 100){
+		//표현식 "273 < 100" 이 참일때 실행 = 거짓이기때문에 당연히 실행안함.
+		 alert('273<100=>true');
+	}
+	alert('종료');    
+*/
+// case 1. 현재 시간이 오후인가? 오전인가?
+function amORpm(){
+    var date = new Date(); // 대문자 객체 = 전역함수
+    // console.log(date);
+    var hour = date.getHours(); //꼬리함수를 쓰려면 변수안에 원한걸 담아서 쓰는것이다.
+    // console.log(typeof(hour));
+    if(hour<12){
+        alert("오전입니다.");
+    }
+    if(hour>=12){
+        alert("오후입니다.");
+    }
+}
+// case 2. 말일인가 아닌가
+function lastDay(){
+    var date = new Date();
+    var day = date.getDate();
+    if(day<30){
+       alert("아직 말일이 아닙니다."); 
+    }else{
+        alert("말일입니다.");
+    }
+}
+
+// !tune
+function lastDayEx(){
+    function endOfMonth(date){
+        return new Date(date.getFullYear(), date.getMonth() + 1,0);
+    }
+    dt = new Date();
+    console.log(endOfMonth(dt).toString());
+}
+
+// todo 1. 현재 시각이 본인 기준으로 아침? 점심? 저녁 먹을 시간인지 알려주는 함수
+function mealTime(){
+    var date = new Date();
+    var hour = date.getHours();
+    if(hour < 11){
+        alert("아침머겅")//아침 먹을 시간대 조건에 들어가야하는 경고창.
+    }else if(hour < 18){
+        alert("점심머겅")//점심 먹을 시간대 조건에 들어가야하는 경고창.
+    }else{
+        alert("그만머겅")//아침 먹을 시간대 조건에 들어가야하는 경고창.
+    }   
+}
+// case 3. 양음수판별식
+function numbJudgment(){
+    var numb = prompt('숫자를 입력하세요', '양,음수를 판별합니다.');
+    if(numb>0){
+        alert("앙수입니다.");
+    }else if(numb<0){
+        alert("음수입니다.");
+    }else if(numb=0){
+        alert("양,음수가 아닌 0 입니다.")
+    }else{
+        alert("잘못된 입력값입니다.");
+    }
+}
+// todo. 홀,짝수 판별식
+function oddOrEven(){
+    var numb = prompt('숫자를 입력하세요' , '짝수, 음수를 판별합니다');
+    if(numb == 0){
+        alert("0은 구분할 수 없습니다")
+    }else if(numb%2 == 0){
+        alert("짝수입니다")// ? 짝수일 때 경고창
+    }else if(numb%2 == 1){
+        alert("홀수입니다")// ? 홀수일 때 경고창
+    }else{
+        alert("올바른 값을 입력하세요")// ? 둘다 아닐 때 경고창
+    }
+}
+
