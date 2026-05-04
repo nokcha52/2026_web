e474fd6de6ae3f5f00b7a59f3705932804a01ef4
nokcha52 <wltn7952@gmail.com>
2026-05-04 10:27:17 +0900

    조건문if기초끝

--- JS/stpe2/js/1_if.js
+++ JS/stpe2/js/1_if.js
@@ -86,3 +86,91 @@ function oddOrEven(){
     }
 }
 
+// case 4. 다음 세 과목의 점수를 입력받고 평균을 구한 후 수,우,미,양,가를 출력하라
+function avgScore(){
+    var koreanScore = Number(prompt("국어 점수를 입력하시오.")); // 한번 더 감싸서 적을 글자를 줄인다.
+    var englishScore = Number(prompt("영어 점수를 입력하시오."));
+    var mathScore = Number(prompt("수학 점수를 입력하시오."));
+    var avg = ((koreanScore + englishScore + mathScore) / 3).toFixed(2);//소숫점 2자리까지다
+    console.log(typeof(avg) + ":" + avg);
+    if(avg > 100 || avg < 0){
+        alert("점수를 똑바로 입력하세요.");
+    }else if(avg >= 90){
+        alert("당신의 성적은 수 입니다.");
+    }else if(avg >= 80){
+        alert("당신의 성적은 우 입니다");
+    }else if(avg >= 70){
+        alert("당신의 성적은 미 입니다");
+    }else if(avg >= 60){
+        alert("당신의 성적은 양 입니다");
+    }else{
+        alert("당신의 성적은 가 입니다.");
+    }
+    /* 
+        todo    
+        1. 90~99 : 수, 80~89 : 우, 70~79 : 미, 60~69: 양, 60미만 : 가. = alert으로 출력되게 만들기     
+    */
+}
+
+// case 5. 사용자 아이디를 입력받아 맞다면(가상의 아이디와 일치하는지) 승인 메세지 출력.
+function userIDCheck(){
+    var userID = prompt("사용자의 아이디");
+    if(userID == "wltn"){
+        alert("비밀번호 입력 페이지로 이동합니다.");
+    }else{
+        alert("잘못된 ID입니다.");
+    }
+}
+
+// todo. 사용자 id "Mark"와 password "1234"를 입력받고 둘다 맞을 경우에만 승인 메세지 출력.
+function userLogin(){
+    var userID = prompt("사용자의 아이디를 입력하세요.")
+    var userPw = prompt("사용자의 비밀번호를 입력하세요.")
+    if(userID == "Mark" && userPw == "1234"){
+        alert("[" + userID + "] 님으로 확인되었습니다.");
+    }else{
+        alert("ID 혹은 Password가 틀렸습니다");
+    }
+}
+// todo. 성공메세지: "Mark님으로 확인되었습니다." -> 변수를 당겨서 [userID]님으로 확인되었습니다.
+// todo. 오류메세지: "ID 혹은 Password가 틀렸습니다."
+
+function userInfoCheck1(){
+    var userID = prompt("사용자의 아이디를 입력하세요.")
+    var userPw = prompt("사용자의 비밀번호를 입력하세요.") 
+    (userID == "Mark" && userPw == "1234")? alert("mark님으로 확인되었습니다.") : alert("ID 혹은 Password가 틀렸습니다");
+    //? (조건식)? (참일 경우):(거짓일 경우);
+}
+
+
+// todo. 사용자 id "Mark" 와 password "1234"의 정보가 틀린 경우 틀린 정도에 대한 오류메시지 출력.
+// todo. 성공메세지: "Mark님으로 확인되었습니다." -> 변수를 당겨서 [userID]님으로 확인되었습니다.
+/* 
+    todo. 
+    오류메세지1. 모든 정보가 맞지 않습니다
+    오류메세지2. 아이디가 존재하지 않습니다.
+    오류메세지 3. 비밀번호가 틀렸습니다
+
+*/
+function userInfoCheck2(){
+    var userID = prompt("사용자의 아이디를 입력하세요.")
+    var userPw = prompt("사용자의 비밀번호를 입력하세요.")
+    // if(userID == "Mark" && userPw == "1234"){
+    //     alert("["+ userID + "] 님으로 확인되었습니다.");
+    // }else if(userID != "Mark" && userPw == "1234"){
+    //     alert("아이디가 존재하지 않습니다.");
+    // }else if(userID == "Mark" && userPw != "1234"){
+    //     alert("비밀번호가 틀렸습니다.");
+    // }else{
+    //     alert("모든 정보가 맞지 않습니다.");
+    // }
+    if(userID == "Mark" && userPw == "1234"){
+        alert("["+ userID + "] 님으로 확인되었습니다.");
+    }else if(userID != "Mark" && userPw == "1234"){
+        alert("아이디가 존재하지 않습니다.");
+    }else if(userID == "Mark" && userPw != "1234"){
+        alert("비밀번호가 틀렸습니다.");
+    }else{
+        alert("모든 정보가 맞지 않습니다.");
+    }
+}
