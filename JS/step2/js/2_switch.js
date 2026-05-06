afe97c6936f5a32dfe391a6b9822abb2ea9fb6de
nokcha52 <wltn7952@gmail.com>
2026-05-06 10:50:02 +0900

    for문 진행중

---
+++ JS/step2/js/2_switch.js
@@ -0,0 +1,86 @@
+
+/*
+?switch
+switch는 조건식의 결과가 case에 걸리는 경우, 비교할때 사용한다.
+( >,<,>=,<=,!= 와 같은 연산자를 사용 안했을 경우에만)
+
+key : 값을 전달받거나 변형 시킬수 있다. 
+ex> a(var) ->key(전달)
+a % 2 (변형)
+
+case : 위에서 전달된 key값과 일치하는 지 비교하는 부분 
+ex> case 0 -> 위에서 전달된 값이 0인지를 판단 하는 부분 
+0이 아니다 -> 다음 case로 넘김.
+
+break : switch는 if와 다르게 한번 실행되면 모든 case를 다 훑게 되어 있기 때문에(그값이 걸리던 안걸리던..) 수동으로 멈춰야 하는데 그것이 break; 
+break는 switch 전유가 아님. 아무데나 쓸수 있음.
+
+default : 위 case에 걸리지 못한 값 (예외처리)일 때  실행되는 부분
+*/
+
+function switchGrammar(){
+    var input = Number(prompt("숫자만 입력"));
+    switch (input%2){
+        case 0:
+            alert("짝수입니다.");
+            break;
+        case 1:
+            alert("홀수입니다.");
+            break;
+        default:
+            alert("숫자가 아닙니다.")
+            break;
+    }
+}
+
+// case 2. lotto 과제를 switch로 변경.
+function lotto(){
+    var numbValue = prompt("입력하실 번호는?");
+    switch (numbValue) {
+        case "1":
+            alert("1등 10억!");
+            break;
+        case "2":
+            alert("2등 5천만원!");
+            break;
+        case "3":
+            alert("3등 3백만원!");
+            break;    
+        default:
+            alert("낙첨되었습니다.");
+            break;
+    }
+}
+
+// todo. swtich문을 이용한 계산기
+// ? 첫번째 숫자와 연산자, 그리고 두번째 숫자를 입력받아 계산해주는 계산기
+function calculator(){
+    /*
+        todo.
+        1. 함수 실행 시 첫번째 숫자를 입력받는 프롬프트 출력.
+        2. 첫번째 숫자를 입력하면 연산자를 입력받는 프롬프트 출력.(단, %나머지 연산자 제외 = 사칙연산만)
+        3. 연사자를 입력하면 두번째 숫자를 받는 입력 프롬프트 출력.
+        4. 1,2,3 모두가 입력되면 수식과 답을 console에 출력 (ex> 10+10=20)
+    */
+   var firstNumb = Number(prompt("첫번째 숫자를 입력하세요."));
+   var cal = prompt("연산자를 입력하세요. +,-,/,* 중");
+   var secondNumb = Number(prompt("두번째 숫자를 입력하세요."));
+
+   switch (cal){
+    case "+":
+        alert(firstNumb + cal + secondNumb + " = " + (firstNumb + secondNumb));
+        break;
+    case "-":
+        alert(firstNumb + cal + secondNumb + " = " +  (firstNumb - secondNumb));
+        break;
+    case "*":
+        alert(firstNumb + cal + secondNumb + " = " +  (firstNumb * secondNumb));
+        break;
+    case "/":
+        alert(firstNumb + cal + secondNumb + " = " +  (firstNumb / secondNumb));
+        break;   
+    default:
+        alert("숫자 혹은 연산자를 잘못 입력했습니다.");
+        break;
+   }
+}
---
+++ JS/step3/js/1_for.js
@@ -0,0 +1,89 @@
+/*
+    ? for
+    * 특정구문을 정해진 횟수 만큼 반복해야 할 경우 사용하는 반복문.
+    실무사용의 예
+    1. 게시판의 게시글 목록 불러올 때
+    2. 구글 맵, 네이버 맵 등의 정보를 출력 할 때
+    3. 메뉴의 항목(관리자제어가능한) 출력 할 때
+    4. 갤러리의 이미지 목록을 출력 할 때
+    5. os 등의 파일 탐색기의 파일 목록 출력 할 때...
+
+    * 루프
+    1. 초기식실행 > 2. 조건식 비교, 조건이 거짓일 경우 반복문 종료 > 
+    3. 참일경우 이하 구문 실행 > 4. 증감부 실행 > 5. 2단계
+
+for (var index = 0; index < 10; index++) {
+    console.log(index);
+    
+}
+*/
+// case 1. 이름을 10번 출력해보자.
+function namePrint10(){
+    var userName = "mark";
+    // document.write('1.'+ userName + '<br)');
+    // document.write('1.'+ userName + '<br)');
+    // document.write('1.'+ userName + '<br)');
+    // document.write('1.'+ userName + '<br)');
+    // document.write('1.'+ userName + '<br)');
+    // document.write('1.'+ userName + '<br)');
+    // document.write('1.'+ userName + '<br)');
+    // document.write('1.'+ userName + '<br)');
+    // document.write('1.'+ userName + '<br)');
+    // document.write('1.'+ userName + '<br)');
+    for(var i = 1; i <= 10; i++){
+        document.write(i + "." + userName + "<br>");
+    }
+}
+// case 2. 이름을 1000번 출력해보자.
+function namePrint1000(){
+    var userName = "mark";
+    for(var i = 1; i <= 1000; i++){
+        document.write(i +"." + userName + "<br>");
+    }
+}
+// todo. 1000번 출력 중, 홀수번째에만 이름이 출력되게 만들기
+function nameprint500(){
+    var userName = "mark";
+    // for(var i = 1; i <= 500; i++){
+    //     var n = 2*i-1;
+    //     document.write(n +". " + userName + "<br>");
+    // }
+    for(var i = 1; i <= 1000; i+=2){
+        document.write(i +". " + userName + "<br>");
+    }
+}
+// case 3. js Engine처럼 생각하기
+function thinkJS(){
+    for(var i = 0; i < 10; i++){ /*? i가 10보다 작을때까지만 반복을 수행.*/
+        document.write("i="+ i, "<br>"); //? 위 조건이 참일 경우 실행한다 = 0~9까지 출력.
+    } // i가 증가하다가 10이 되는 순간 break.
+    document.write("종료 i="+ i); // ? for loop가 중지될 때 i의 값인 10이 출력.
+    // loop문 : 조건을 잘못 짤 경우 -> 무한반복(infinite loop)에 빠짐 = 중지 불가능 = server를 꺼야할 경우에는 큰일남. 재부팅하는데 시간이 최소 10분 가량
+}
+
+// todo. 1-10까지 출력되는 단순 반복문을 여러개 만들어보기
+function answer1(){
+    for(var i = 1; i <= 10; i+=2){ //? 10보다 작거나 같을 때 (1~10)까지만 반복 수행. 단, 반복이 끝난 후 i에 2를 더함
+        document.write(i+"<br>"); // 증감이 이루어지지 않은 1,3,5,7,9
+        document.write((i+1)+"<br>"); // 증감이 이루어진 2,4,6,8,10
+    }
+}
+function answer2(){
+    for(var i = 1; i <= 5; i++){
+        document.write((2*i-1)+"<br>");
+        document.write((2*i)+"<br>");
+    }
+}
+function answer3(){
+    for(var i = 10; i > 0; i--){
+        document.write((11-i) + "<br>");
+        console.log(i);
+        continue;
+        document.write(i+"<br>");
+    }
+}
+function answer4(){
+    for(var i = 10; i >= 1; i--){
+        document.write((11-i) + "<br>");
+    }
+}
